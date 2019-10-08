e0775b5fcf5b862e0e7fc1171a0cdc81fa9a8f1a
elvigourou <elvigourou@turing.local.isima.fr>
2019-10-08 16:53:41 +0200

    Changement de style

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,27 +14,37 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+        for (int i = 0; i < items.length; i++)
+        {
+            if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (items[i].quality > 0)
+                {
+                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) 
+                    {
                         items[i].quality = items[i].quality - 1;
                     }
                 }
-            } else {
-                if (items[i].quality < 50) {
+            }
+            else
+            {
+                if (items[i].quality < 50)
+                {
                     increaseQuality(items[i]);
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
+                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert"))
+                    {
+                        if (items[i].sellIn < 11)
+                        {
+                            if (items[i].quality < 50)
+                            {
                                 increaseQuality(items[i]);
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellIn < 6)
+                        {
+                            if (items[i].quality < 50)
+                            {
                                 increaseQuality(items[i]);
                             }
                         }
@@ -42,23 +52,34 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros"))
+            {
                 items[i].sellIn = items[i].sellIn - 1;
             }
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (items[i].sellIn < 0) 
+            {
+                if (!items[i].name.equals("Aged Brie")) 
+                {
+                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert"))
+                    {
+                        if (items[i].quality > 0)
+                        {
+                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros"))
+                            {
                                 items[i].quality = items[i].quality - 1;
                             }
                         }
-                    } else {
+                    }
+                    else
+                    {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
-                } else {
-                    if (items[i].quality < 50) {
+                }
+                else
+                {
+                    if (items[i].quality < 50)
+                    {
                         increaseQuality(items[i]);
                     }
                 }
