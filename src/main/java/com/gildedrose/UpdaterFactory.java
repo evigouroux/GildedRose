a6ab7a57cfa0505b13525832f6f60c7c1373ff13
Fuuf <evigouroux43@gmail.com>
2019-11-14 15:55:04 +0100

    Création de la méthode de création de UpdaterFactory

--- src/main/java/com/gildedrose/UpdaterFactory.java
+++ src/main/java/com/gildedrose/UpdaterFactory.java
@@ -2,5 +2,27 @@ package com.gildedrose;
 
 public class UpdaterFactory
 {
-
+    public static Updater getUpdater (Item item)
+    {
+        if (item.Name.Contains("Sulfuras"))
+        {
+            return new LegendaryUpdater();
+        }
+        else if (item.Name.Contains("Aged Brie"))
+        {
+            return new ImprovedWithAgeUpdater();
+        }
+        else if (item.Name.Contains("Backstage pass"))
+        {
+            return new BackstagePassUpdater();
+        }
+        else if (item.Name.Contains("Conjured"))
+        {
+            return new ConjuredUpdater();
+        }
+        else
+        {
+            return new RegularItemUpdater();
+        }
+    }
 }
