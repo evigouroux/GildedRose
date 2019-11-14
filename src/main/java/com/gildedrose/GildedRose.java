aa5a40a432386bbaad7368c01282ec8b73a397d3
Fuuf <evigouroux43@gmail.com>
2019-11-14 16:38:13 +0100

    Supression de vieilles variables devenues obsolètes

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -2,16 +2,7 @@ package com.gildedrose;
 
 class GildedRose {
 
-    Item[] items;
-    private static final int MIN_QUALITY = 0;
-    private static final int MAX_QUALITY = 50;
-    private static final int MIN_SELLIN = 0;
-    private static final int CONCERT_GETS_NEAR = 10;
-    private static final int CONCERT_GETS_VERY_NEAR = 5;
-
-    private static final String AGED_BRIE = "Aged Brie";
-    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";    
+    Item[] items;  
 
     public GildedRose(Item[] items) {
         this.items = items;
