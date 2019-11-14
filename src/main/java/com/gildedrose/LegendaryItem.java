de29e3692b11edcf8de5cb70bbd97e23465c1223
Fuuf <evigouroux43@gmail.com>
2019-11-14 15:04:36 +0100

    Changement de pc et test

---
+++ src/main/java/com/gildedrose/LegendaryItem.java
@@ -0,0 +1,12 @@
+package com.gildedrose;
+
+public class LegendaryItem extends Item
+{
+
+   public LegendaryItem (String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+}
